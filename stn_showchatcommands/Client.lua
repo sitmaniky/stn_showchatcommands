888fbdd05d17f5e5bbdf6cfe8673ad93be3e8c80
ToKyoV2 <88289890+ToKyoV2@users.noreply.github.com>
2022-03-12 22:47:18 +0100

    Add files via upload

---
+++ stn_showchatcommands/Client.lua
@@ -0,0 +1,8 @@
+
+TriggerEvent('chat:addSuggestion', '/me', 'Napriklad /me podava doklady', {})
+TriggerEvent('chat:addSuggestion', '/do', 'Napriklad /do je pan hledany?', {})
+TriggerEvent('chat:addSuggestion', '/doc', '/doc a cislo kolikchcete', {})
+TriggerEvent('chat:addSuggestion', '/try', 'Rozhodne zda ANO nebo NE', {})
+TriggerEvent('chat:addSuggestion', '/police', 'Slouzi pro oznameni LSPD musis pracovat u LSPD', {})
+TriggerEvent('chat:addSuggestion', '/ems', 'Slouzi pro oznameni EMS musis pracovat u EMSEK', {})
+
